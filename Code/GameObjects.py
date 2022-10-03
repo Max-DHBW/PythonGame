11a89d5a8e289e8b464ad7038cd6378c37c10571
Max-DHBW <kurtenma@dhbw-loerrach.de>
2022-10-03 12:14:54 +0200

    Rabbit.Class angefangen und Gradd.egg hinzugefügt

--- Code/GameObjects.py
+++ Code/GameObjects.py
@@ -1,24 +1,79 @@
-from panda3d.core import Vec3, Vec2
+from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
-from panda3d.core import CollisionSphere, CollisionNode
+from panda3d.core import Vec3, Vec2
+import random as rand
 
+class Rabbit():
+    def __init__(self, pos, speed):
+        self.rabbit = Actor("../Models/rabbit", {})
+        self.rabbit.reparentTo(render)
+        self.rabbit.setPos(pos)
+        self.speed = speed
 
-class GameObject():
-    def __init__(self, pos, modelName, modelAnims):
-        self.actor = Actor(modelName, modelAnims)
-        self.actor.reparentTo(self.render)
-        self.actor.setPos(pos)
+        # Initialisirung
+        self.direction = Vec3(0, 0, 0)
+        self.direction_next_direction = (0, 1, 2, 3)
+        self.has_moved = rand.randint(20, 50)
+        self.random_direction(self.direction_next_direction)
+
 
     def update(self):
-        pass
+        self.rabbit.setPos(self.move())
+
+    def move(self):
+        vector_pos = self.rabbit.getPos()
+
+        if self.has_moved == 0:
+            self.random_direction(self.direction_next_direction)
+            self.has_moved = rand.randint(20, 50)
+        else:
+            self.has_moved = self.has_moved - 1
+
+        vector_pos_destiny = vector_pos + self.direction
+        return self.check_if_map_border(vector_pos_destiny, vector_pos)
+
+    def check_if_map_border(self, vector, vector_pos):
+        if vector.getX() >= 95:
+            self.random_direction((1, 2, 3))
+            self.has_moved = rand.randint(20, 50)
+            return vector_pos + self.direction
+        if vector.getX() <= -95:
+            self.random_direction((0, 2, 3))
+            self.has_moved = rand.randint(20, 50)
+            return vector_pos + self.direction
+        if vector.getY() >= 95:
+            self.random_direction((0, 1, 2))
+            self.has_moved = rand.randint(20, 50)
+            return vector_pos + self.direction
+        if vector.getY() <= -95:
+            self.random_direction((0, 1, 3))
+            self.has_moved = rand.randint(20, 50)
+            return vector_pos + self.direction
+
+        return vector
+
+    def random_direction(self, choice):
+        number = rand.choice(choice)
+        match number:
+            case 0:
+                self.direction = Vec3(self.speed, 0, 0)
+                self.direction_next_direction = (0, 2, 3)
+                self.rabbit.setH(0)
+            case 1:
+                self.direction = Vec3(-self.speed, 0, 0)
+                self.direction_next_direction = (1, 2, 3)
+                self.rabbit.setH(180)
+            case 2:
+                self.direction = Vec3(0, -self.speed, 0)
+                self.direction_next_direction = (2, 0, 1)
+                self.rabbit.setH(-90)
+            case 3:
+                self.direction = Vec3(0, self.speed, 0)
+                self.direction_next_direction = (3, 0, 1)
+                self.rabbit.setH(90)
 
     def delete(self):
-        pass
-
-
-class Plant(GameObject):
-    pass
-
-
-class Bunny(GameObject):
-    pass
+        if self.rabbit is not None:
+            self.rabbit.cleanup()
+            self.rabbit.removeNode()
+            self.rabbit = None
--- Code/main.py
+++ Code/main.py
@@ -1,25 +1,53 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
 from light_setup import setup_point_light
+from direct.task import Task
+from panda3d.core import Vec3, Vec2
+from GameObjects import *
+
+from GameObjects import *
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        # Kamera und Background
         self.set_background_color(0, 0, 0, 1)
-        self.cam.setPos(0, -700, 60)
+        self.cam.setPos(0, -275, 150)
+        self.cam.setP(-30)
 
+        # Grundplatte
         self.floor = self.loader.loadModel("../Models/ground")
+        self.floor.setPos(0, 0, -0.5)
+        self.floor.setScale(0.5)
         self.floor.reparentTo(self.render)
 
+        # Window
         properties = WindowProperties()
         properties.setSize(2000, 1200)
         self.win.requestProperties(properties)
 
+        # Licht
         setup_point_light(self.render, (30, 0, 100))
 
-        #Koordinaten Eckpunkte
-        #print(self.floor.getTightBounds())
+        #Koordinaten Eckpunkte (Test)
+        print(self.floor.getTightBounds())
+
+        # Update Loop Registrierung
+        self.updateTask = self.taskMgr.add(self.update, "update")
+
+        # Objekte Spawnen
+        self.rabbits = []
+        self.spawn_rabbits(10)
+
+    def update(self, task):
+        [rabbit.update() for rabbit in self.rabbits]
+        return task.cont
+
+    def spawn_rabbits(self, count):
+        while count != 0:
+            self.rabbits.append(Rabbit(Vec3(5, 0, 0), 0.1))
+            count = count - 1
 
 game = Game()
 game.run()
